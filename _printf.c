63ab4013f73fd546b477394384c7be026b9db54a
domgeomik <domgeomik@gmail.com>
2023-08-20 08:42:55 -0700

    0x11. C - printf team project by Michael Dominic and Emmanuel Onyemaechi

--- _printf.c
+++ _printf.c
@@ -1,13 +1,13 @@
 #include "main.h"
 void print_buffer(char buffer[], int *buff_ind);
 /**
-* _printf - Printf Function
+* _printf - Printf function created by my team
 * @format: format.
-* Return: Print characters.
+* Return: Printed characters.
 */
 int _printf(const char *format, ...)
 {
-int i, printed = 0, printed_chars = 0;
+int i, printed_Value = 0, chars_Printed = 0;
 int flags, width, precision, size, buff_ind = 0;
 va_list list;
 char buffer[BUFF_SIZE];
@@ -22,7 +22,7 @@ buffer[buff_ind++] = format[i];
 if (buff_ind == BUFF_SIZE)
 print_buffer(buffer, &buff_ind);
 /*write(1, &format[i], 1);*/
-printed_chars++;
+chars_Printed++;
 }
 else
 {
@@ -32,21 +32,21 @@ width = get_width(format, &i, list);
 precision = get_precision(format, &i, list);
 size = get_size(format, &i);
 ++i;
-printed = handle_print(format, &i, list, buffer,
+printed_Value = handle_print(format, &i, list, buffer,
 flags, width, precision, size);
-if (printed == -1)
+if (printed_Value == -1)
 return (-1);
-printed_chars += printed;
+chars_Printed += printed_Value;
 }
 }
 print_buffer(buffer, &buff_ind);
 va_end(list);
-return (printed_chars);
+return (chars_Printed);
 }
 /**
-* print_buffer - Prints the buffer if it exist
-* @buffer: Array of characterss
-* @buff_ind: Index to add next characters
+* print_buffer - Prints the values of the buffer if it exist
+* @buffer: Array of characters
+* @buff_ind: Index where to add next characters, represents the length.
 */
 void print_buffer(char buffer[], int *buff_ind)
 {
